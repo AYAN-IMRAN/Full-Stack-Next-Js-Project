cc58532630fd7e2808c4f3a04557f7b4fd50aa2a
AYAN-IMRAN <ayanimranayanahmed@gmail.com>
2026-02-27 19:42:54 +0500

    ImageKit Connected Done

---
+++ app/api/video/route.ts
@@ -0,0 +1,75 @@
+import { authOptions } from "@/lib/auth";
+import { ConnectToDB } from "@/lib/db";
+import Video, { IVideo } from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+
+
+
+
+async function GET() {
+    try {
+        await ConnectToDB();
+        const video = await Video.find({}).sort({ createdAt: -1 }).lean();
+
+        if (!video || video.length === 0) {
+            return NextResponse.json([], { status: 200 })
+        }
+
+        return NextResponse.json(video)
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to fetch videos" },
+            { status: 500 }
+        );
+    }
+
+}
+
+
+async function POST(requst: NextRequest) {
+    try {
+        const session = await getServerSession(authOptions)
+
+
+        if (!session) {
+            return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+        }
+
+        await ConnectToDB()
+
+        const body: IVideo = await requst.json();
+
+        if (
+            !body.title ||
+            !body.description ||
+            !body.videoUrl ||
+            !body.thumbnailUrl
+        ) {
+            return NextResponse.json(
+                { error: "Missing required fields" },
+                { status: 400 }
+            );
+        }
+
+        const videoData = {
+            ...body,
+            controls: body?.controls ?? true,
+            transformation: {
+                height: 1920,
+                width: 1080,
+                quality: body.transformation?.quality ?? 100,
+            },
+        };
+        const newVideo = await Video.create(videoData);
+
+        return NextResponse.json(newVideo)
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to create video" },
+            { status: 500 }
+        );
+    }
+
+}
